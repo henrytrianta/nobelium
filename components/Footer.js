8ca4b298127dd4cf3fee2ba59d9f929765afcd72
henrytrianta <henry.trianta@gmail.com>
2021-08-26 00:42:41 +0800

    feat: remove footer and add favicon

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,4 @@
 import BLOG from '@/blog.config'
-import Vercel from '@/components/Vercel'
 const Footer = ({ fullWidth }) => {
   const d = new Date()
   const y = d.getFullYear()
@@ -12,11 +11,10 @@ const Footer = ({ fullWidth }) => {
     >
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
-        <div className="flex align-baseline justify-between flex-wrap">
+        <div className="flex flex-wrap justify-between align-baseline">
           <p>
             © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
-          <Vercel />
         </div>
       </div>
     </div>
